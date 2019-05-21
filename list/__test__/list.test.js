1cf5a7261121a00c1af6208c4f8845a27bb7538b
JeffLawrence1 <jefflaw13@hotmail.com>
2019-05-21 16:36:18 -0700

    work on errors

--- list/__test__/list.test.js
+++ list/__test__/list.test.js
@@ -1,8 +1,8 @@
 'use strict';
 
 // These 2 should be interchangeable!
-// const List = require('../list.js');
-const List = require('../list-constructor.js');
+const List = require('../list.js');
+// const List = require('../list-constructor.js');
 
 describe('List Data Structure', () => {
 
@@ -38,6 +38,28 @@ describe('List Data Structure', () => {
     stuff.shift();
     expect(stuff.length).toEqual(2);
     expect(stuff.data[1]).toEqual('b');
+  });
+
+  it('adds items at the beginning of the data set', () => {
+    let stuff = new List();
+    stuff.push('a');
+    stuff.push('b');
+    stuff.push('c');
+    stuff.unshift(1);
+    expect(stuff.length).toEqual(4);
+    expect(stuff.data[0]).toEqual(1);
+  });
+
+  it('do something to each item of the data set', () => {
+    let stuff = new List();
+    stuff.push(1);
+    stuff.push(2);
+    stuff.push(3);
+    stuff.foreach((item) => {
+      return item*2;
+    });
+    expect(stuff.length).toEqual(3);
+    expect(stuff.data[0]).toEqual(2);
   });
 
 });
--- list/list.js
+++ list/list.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// const errorHandler = require('./error-handler.js');
+
 class List {
 
   constructor() {
@@ -11,6 +13,7 @@ class List {
    * Add item to the end of the list
    * @param item
    */
+
   push(item) {
     // Add an item to the end
     this.data[this.length] = item;
@@ -21,11 +24,34 @@ class List {
    * // Remove an item from the end of the list and return it's value
    * @returns {*}
    */
+
   pop() {
     let returnValue = this.data[this.length];
     delete this.data[this.length];
     this.length--;
     return returnValue;
+  }
+
+  shift() {
+    let returnValue = this.data[0];
+    delete this.data[0];
+    this.length--;
+    return returnValue;
+  }
+
+  unshift(item) {
+    for(let i = this.length; i > 0; i--){
+      this.data[i] = this.data[i - 1];
+    }
+    this.data[0] = item;
+    this.length++;
+    return this.length;
+  }
+
+  foreach(callback) {
+    for(let i = 0; i < this.length; i++){
+      this.data[i] = callback(this.data[i]);
+    }
   }
 
 }
